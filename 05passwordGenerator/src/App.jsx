a1cd14dfe44de81f46674701310f2775c0ac73de
Anurag Kandulna <dev.ak@outlook.in>
2024-08-11 00:39:01 +0530

    added other elements

--- 05passwordGenerator/src/App.jsx
+++ 05passwordGenerator/src/App.jsx
@@ -30,16 +30,56 @@ function App() {
   return (
     <>
       <div className='w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700'>
-        <h1 className='text-white text-center'>Password Generator</h1>
+        <h1 className='text-white text-center m-3'>Password Generator</h1>
         <div className='className="flex shadow rounded-lg overflow-hidden mb4"'>
           <input 
             type='text'
             value={password}
-            className='outline-none w-full py-1 px-3' 
+            className='outline-none w-full py-1 px-3 rounded-md' 
             placeholder='password' 
             readOnly
-            
             />
+          <div className='py-1'></div>
+          <button className='rounded-md outline-none bg-blue-700 text-white px-3 py-1'>Copy</button>
+        </div>
+
+        <div className='flex text-sm gap-x-2'>
+          <div className='flex items-center gap-x-1'>
+            <input 
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className='curser-pointer'
+              onChange={(e) => {setLength(e.target.value)}}
+            />
+            <label>Length: {length}</label>
+          </div>
+
+          <div className='flex items-center gap-x-1'>
+            <input 
+              type="checkbox"
+              defaultChecked={allowNum}
+              id="nummberInput"
+              onChange={() => {
+                setAllowNum((prev) => !prev);
+              }}
+            />
+            <label htmlFor='numberInput'>Numbers</label>
+          </div>
+
+          <div className='flex items-center gap-x-1'>
+            <input 
+              type="checkbox"
+              defaultChecked={allowChar}
+              id="charInput"
+              onChange={() => {
+                setAllowNum((prev) => !prev);
+              }}
+            />
+            <label htmlFor='charInput'>Characters</label>
+          </div>
+
         </div>
       </div>
       
