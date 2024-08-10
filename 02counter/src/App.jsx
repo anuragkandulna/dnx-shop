7ad38ebbfe06fb5b9a82428babb6b38eaf2efdeb
Anurag Kandulna <dev.ak@outlook.in>
2024-08-10 13:43:26 +0530

    updated code to test batched updated in 02 counter app

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -12,24 +12,39 @@ function App() {
   // let counter = 15
 
   const addValue = () => {
-    if (counter < 20) {
-      counter = counter + 1
-      setCounter(counter)
-      console.log('Updated value after increment: ', counter)
-    }
-    else {
-      console.log('Cannot increment more.')
-    }
+
+    // This repetition wont work because React useState() updates the values in batches and
+    // if repetition is found then it conconsiders the same work and send it in a single batch,
+    // hence value is 5 and not 8. This is feature of fibre that gives more control over the passed value.
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+
+
+    // To do the repeated updates, fetch the previous counter value and increment on top of it. 
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+    setCounter((prevCounter) => prevCounter + 1)
+
+
+    // if (counter < 20) {
+    //   counter = counter + 1
+    //   setCounter(counter)
+    //   console.log('Updated value after increment: ', counter)
+    // }
+    // else {
+    //   console.log('Cannot increment more.')
+    // }
   }
 
   const removeValue = () => {
-    if (counter > 0) {
-      setCounter(counter - 1) 
-      console.log('Updated value after decrement: ', counter)
-    }
-    else {
-      console.log('Cannot decrement further.')
-    }
+    // if (counter > 0) {
+    //   setCounter(counter - 1) 
+    //   console.log('Updated value after decrement: ', counter)
+    // }
+    // else {
+    //   console.log('Cannot decrement further.')
+    // }
     
   }
 
