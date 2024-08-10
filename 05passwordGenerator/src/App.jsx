41adcfeb0daeb20adfb4d0d421c83be91665735b
Anurag Kandulna <dev.ak@outlook.in>
2024-08-10 21:09:21 +0530

    password generator initial commit

---
+++ 05passwordGenerator/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-full h-screen duration-200' style={{backgroundColor: 'blue'}}>
+      <h1  className='text-white shadow-lg'>Password Generator</h1>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
