2af3325073cdf5a21ebf1e61bf4e9d050da9effe
Anurag Kandulna <dev.ak@outlook.in>
2024-08-20 00:03:15 +0530

    Router integration code

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -1,15 +1,16 @@
 import { useState } from 'react'
-
-
+import './App.css'
+// import Header from './components/Header/Header'
+// import Footer from './components/Footer/Footer'
+// import Home from './components/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     
+      
     </>
   )
 }
