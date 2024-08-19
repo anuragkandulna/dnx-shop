bb24ae7eba111423534d71125ea9e9e27d91ed48
Anurag Kandulna <dev.ak@outlook.in>
2024-08-19 15:12:10 +0530

    Added bindings using tab

--- 06currencyConverter/src/components/InputBox.jsx
+++ 06currencyConverter/src/components/InputBox.jsx
@@ -12,18 +12,20 @@ function InputBox({
 
   className = "",
 }) {
- 
+    // This is optional. UseId hook.
+    const amountInputId = useId()
 
   // e.target.value will return string so need to convert to number
   // If running loop on react component then pass key, so that performance hit is not taken.
   return (
       <div className={`bg-white p-3 rounded-lg text-sm flex ${className}`}>
           <div className="w-1/2">
-              <label  className="text-black/40 mb-2 inline-block">
+              <label htmlFor={amountInputId}
+              className="text-black/40 mb-2 inline-block">
                   {label}
               </label>
               <input
-                  
+                  id={amountInputId}
                   className="outline-none w-full bg-transparent py-1.5"
                   type="number"
                   placeholder="Amount"
