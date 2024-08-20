84ea9bb6d1edc15352be7176f1a61e2b345e002f
Anurag Kandulna <dev.ak@outlook.in>
2024-08-20 11:13:49 +0530

    fixed multiple issues

--- 07reactRouter/src/App.jsx
+++ 07reactRouter/src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 // import Home from './components/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
--- 07reactRouter/src/Layout.jsx
+++ 07reactRouter/src/Layout.jsx
@@ -1,8 +1,8 @@
 // Can be named as Route.jsx
 
 import React from 'react'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
+import Header from '../components/Header/Header'
+import Footer from '../components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
 
 function Layout() {
