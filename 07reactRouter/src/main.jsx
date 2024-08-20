21ee4770115da157173b4ea13d532495b1046c49
Anurag Kandulna <dev.ak@outlook.in>
2024-08-20 11:47:07 +0530

    Restored back to hc code

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,8 +8,25 @@ import Home from '../components/Home/Home.jsx'
 import About from '../components/About/About.jsx'
 
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      },
+    ]
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
