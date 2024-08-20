e1dab0aa2fcb3c633a039927d0a6eae37db5b747
Anurag Kandulna <dev.ak@outlook.in>
2024-08-20 11:40:54 +0530

    Fixed some issues

--- 07reactRouter/src/main.jsx
+++ 07reactRouter/src/main.jsx
@@ -8,25 +8,8 @@ import Home from '../components/Home/Home.jsx'
 import About from '../components/About/About.jsx'
 
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Layout/>,
-    children: [
-      {
-        path: "",
-        element: <Home/>
-      },
-      {
-        path: "about",
-        element: <About/>
-      }
-    ]
-  }
-])
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </StrictMode>,
 )
